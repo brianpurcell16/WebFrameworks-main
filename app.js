362177c364d7e9688726ab39cdd6fb56a46ba518
brianpurcell16 <brian.purcell16@gmail.com>
2024-10-27 11:50:16 +0000

    commented broken stuff

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
 
-const apiRoutes = require('./app_api/routes/index');
+//const apiRoutes = require('./app_api/routes/index');
 const index = require('./app_server/routes/index');
 
 
@@ -26,7 +26,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/api', apiRoutes);
+//app.use('/api', apiRoutes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
